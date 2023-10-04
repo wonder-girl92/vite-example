4df25bae34c0618839171a97ee6d19101b4bfe98
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2023-10-05 01:13:26 +0300

    bootstrap

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Message(props) {
+    const name = 'Razet';
+    if (name)
+        return (
+        <h1>hello {name}</h1> );
+    return <h1>hello world</h1>;
+}
+
+export default Message;
